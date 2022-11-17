279f585cf57b4d99c41fd60df84df9860812c0e0
louis1001 <louis1001dev@gmail.com>
2022-11-16 21:50:02 -0600

    Initial Commit

---
+++ hangul-practice/hangul_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangul_practiceApp.swift
+//  hangul-practice
+//
+//  Created by Luis Gonzalez on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct hangul_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
